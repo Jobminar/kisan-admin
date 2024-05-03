6baa3b8b889af5538db5ee1fc651ead4b27d121d
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-05-03 16:07:19 +0530

    added the reports to App.jsx

--- client/src/components/Admin/reports.jsx
+++ client/src/components/Admin/reports.jsx
@@ -1,23 +1,34 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Reports = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
+  const handleNavigate = () => {
+    navigate("https://dashboard.razorpay.com/signin?screen=sign_in");
+  };
 
   return (
     <>
-        <div style={{display:'flex', justifyContent:'center' , alignItems:'center' , height:'70vh' , font:'Bold',fontSize:'50px'}}
-          onClick={() => {
-            // Navigate to the Reports URL
-            window.location.href =
-              "https://dashboard.razorpay.com/signin?screen=sign_in";
-          }}
-        >
-          <img src='http://images.deccanchronicle.com/dc-Cover-u0b349upqugfio195s4lpk8144-20190213120303.Medi.jpeg' alt='razorpay' style={{width:'300px' , height:'200px'}}/>
-        </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "70vh",
+          fontWeight: "bold",
+          fontSize: "50px",
+        }}
+        onClick={handleNavigate}
+      >
+        <img
+          src="http://images.deccanchronicle.com/dc-Cover-u0b349upqugfio195s4lpk8144-20190213120303.Medi.jpeg"
+          alt="razorpay"
+          style={{ width: "300px", height: "200px" }}
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Reports
+export default Reports;
