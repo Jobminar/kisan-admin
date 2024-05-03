1845f4e22e86e62c9e71292b37425ddbe00b53ea
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-05-03 16:19:22 +0530

    rectified reports

--- client/src/components/Admin/reports.jsx
+++ client/src/components/Admin/reports.jsx
@@ -1,13 +1,4 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
 const Reports = () => {
-  const navigate = useNavigate();
-
-  const handleNavigate = () => {
-    navigate("https://dashboard.razorpay.com/signin?screen=sign_in");
-  };
-
   return (
     <>
       <div
@@ -16,10 +7,14 @@ const Reports = () => {
           justifyContent: "center",
           alignItems: "center",
           height: "70vh",
-          fontWeight: "bold",
+          font: "Bold",
           fontSize: "50px",
         }}
-        onClick={handleNavigate}
+        onClick={() => {
+          // Navigate to the Reports URL
+          window.location.href =
+            "https://dashboard.razorpay.com/signin?screen=sign_in";
+        }}
       >
         <img
           src="http://images.deccanchronicle.com/dc-Cover-u0b349upqugfio195s4lpk8144-20190213120303.Medi.jpeg"
